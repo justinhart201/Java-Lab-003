e6c48ca12b411e3c45f43aa89c1cdfb0ffe9b258
Justin hart <justinhart201@outlook.com>
2023-10-19 11:40:07 -0700

    lab3

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -1,14 +1,17 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import the Scanner class
 
-public class GettingHotInHere {
-    /**
-     * Teaching Mr. Roboto how to listen to us.
-     * @param args Command line arguments [The source file path, The target file path, ...]
-     **/
+class TemperatureConverter {
     public static void main(String[] args) {
-        // The instantiation code for a Scanner instance.
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("What is the temperature in °F: ");
-        int number = scanner.nextInt();
+        Scanner input = new Scanner(System.in); // Create a Scanner object
+        double fahrenheit, celsius; // Declare two variables of double type
+        System.out.print("Enter temperature in Fahrenheit: "); // Prompt the user to enter temperature in Fahrenheit
+        fahrenheit = input.nextDouble(); // Read user input as a double value
+        celsius = (fahrenheit - 32) * 5.0 / 9.0; // Calculate celsius value using the formula
+        System.out.println("The " + fahrenheit + "°F you typed"); // Print fahrenheit value
+        System.out.println("The " + celsius + "°C you converted as a floating point number"); // Print celsius value as a floating point number
+        System.out.println("The " + (int) celsius + "°C as an integer"); // Print celsius value as an integer
+        System.out.println("If it were 2°C warmer it would be: " + (celsius + 2) + "°C"); // Print the string with some arithmetic and concatenation operations
+        input.close(); // Close the input object
     }
 }
+
--- src/MrRoboto.java
+++ src/MrRoboto.java
@@ -1,19 +1,12 @@
-// Import the Scanner tool used for reading (All external packages/libraries/apis are imported similarly this)
-import java.util.Scanner;
+import java.util.Scanner; // Import the Scanner class
 
 public class MrRoboto {
-    /**
-     * Teaching Mr. Roboto how to listen to us.
-     * @param args Command line arguments [The source file path, The target file path, ...]
-     **/
     public static void main(String[] args) {
-        // The instantiation code for a Scanner instance.
-        Scanner scanner = new Scanner(System.in);
-
-        // Concatenate the user response to this String.
         String domo = "Domo arigato ";
-
-        System.out.print("My name is Mr. Roboto, are you Kilroy? ");
-        // Write your program here
+        Scanner scanner = new Scanner(System.in); // Create a Scanner object
+        System.out.println("My name is Mr. Roboto, are you Kilroy?");
+        String userInput = scanner.nextLine(); // Read user input
+        System.out.println(userInput + domo); // Output user input concatenated with domo
+        scanner.close(); // Close the scanner object
     }
 }
